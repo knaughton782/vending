c76c7c4606a460e86a4488196297a484c404ddef
Bri <brittneystuart2613@gmail.com>
2020-01-14 23:17:08 -0700

    added css styles with flex and added candy bars

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 function candyPicker(location, payment) {
-    console.log(location);
-    // let location = document.getElementById('location').value;
-    // let payment = document.getElementById('payment').value;
-    console.log(payment);
-    let price;
-    // let display;
-    
-    if (location == 'A1') {
-        price = 2.00;
-        let calcChange = payment - price;
-        console.log('your change is: ' + calcChange);
-        return calcChange;
-        
-    }
+  // let location = document.getElementById('location').value;
+  // let payment = document.getElementById('payment').value;
+  let price;
+  // let display;
 
-
+  if (location == "A1") {
+    price = 2.0;
+    let calcChange = payment - price;
+    console.log("your change is: " + calcChange);
+    return calcChange;
+  } else if (location == "B1") {
+    price = 3.5;
+    let calcChange = payment - price;
+    console.log("your change is: " + calcChange);
+    return calcChange;
+  } else if (location == "C1") {
+    price = 4.0;
+    let calcChange = payment - price;
+    console.log("your change is: " + calcChange);
+    return calcChange;
+  } else if (location == "C2") {
+    price = 1.5;
+    let calcChange = payment - price;
+    console.log("your change is: " + calcChange);
+    return calcChange;
+  } else if (location == "D1") {
+    price = 3.5;
+    let calcChange = payment - price;
+    console.log("your change is: " + calcChange);
+    return calcChange;
+  } else {
+    return `Your selection was invalid please try again`;
+  }
 }
-candyPicker('A1', 4);
+candyPicker("B1", 6);
+candyPicker("D1", 4);
+candyPicker("C1", 4);
